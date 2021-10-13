a1fa1a2fcd23c2d77d5221fb7fdf77b030045373
Filip Manole <filip.manole@gmail.com>
2021-10-13 22:09:25 +0300

    project setup: hardhat; waffle; ethers-v5; typechain;

---
+++ hardhat.config.ts
@@ -0,0 +1,8 @@
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export const solidity = "0.8.9";
