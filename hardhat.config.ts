0f0f34147d64f03d9d9e1f921fe8ce7de37da487
Filip Manole <filip.manole@gmail.com>
2021-10-17 23:31:11 +0300

    Explicit hardhat configs; typechain config to generate types for used external artifacts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,4 +5,35 @@ import '@nomiclabs/hardhat-waffle';
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-export const solidity = "0.8.9";
+export default {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: [
+      'node_modules/@openzeppelin/contracts/build/contracts/ERC20PresetMinterPauser.json',
+      'node_modules/@uniswap/v2-core/build/UniswapV2Factory.json',
+    ],
+  },
+}
+
