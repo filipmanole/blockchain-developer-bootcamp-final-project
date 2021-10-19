3d419ac63b29883be979bee09808a64c90e707f4
Filip Manole <filip.manole@gmail.com>
2021-10-20 00:46:21 +0300

    Draft implementation of Swapper contract;

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -1,20 +1,91 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Swapper {
-  event PairAdded(address token0, address token1);
-  event Swapped(address token, uint ammount);
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+import '@uniswap/v2-periphery/contracts/interfaces/IWETH.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@uniswap/v2-periphery/contracts/interfaces/IERC20.sol';
+import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
+
+contract Swapper is Ownable { 
+  uint private tokensLen;
+  mapping(uint => address) private tokens;
+  mapping(address => bool) private exist;
+
+  IUniswapV2Factory public immutable factory;
+  IUniswapV2Router02 public immutable router;
+  IWETH public immutable WETH;
+
+  event PairAdded(uint token0amount, uint token1amount, uint liquidity);
+  event Swapped();
   event Withdrawn();
 
-  constructor(address _factory) {}
+  constructor(address _factory, address _router) {
+    factory = IUniswapV2Factory(_factory);
+    router = IUniswapV2Router02(_router);
+    WETH = IWETH(router.WETH());
 
-  function addLiquidity(address token0, uint token0amount, address token1, uint token1amount) public {}
+    tokensLen = 0;
+  }
 
-  function swap(address tokenIn, address tokenOut, uint amountIn, uint amountOutMin) public {}
+  function addLiquidity(address token0, uint token0amount, address token1, uint token1amount) public {
+    require(IERC20(token0).transferFrom(msg.sender, address(this), token0amount), 'transfer failed');
+    require(IERC20(token1).transferFrom(msg.sender, address(this), token1amount), 'transfer failed');
 
-  function withdraw() public {}
+    require(IERC20(token0).approve(address(router), token0amount), 'approve failed');
+    require(IERC20(token1).approve(address(router), token1amount), 'approve failed');
 
-  function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts) {}
+    (uint amount0, uint amount1, uint liquidity) = 
+      router.addLiquidity(token0, token1, token0amount, token1amount, 1, 1, address(this), block.timestamp);
 
-  function getAmountsIn(uint amountOut, address[] memory path) public view returns (uint[] memory amounts) {}
+    emit PairAdded(amount0, amount1, liquidity);
+  }
+
+  function swap(address tokenIn, address tokenOut, uint amountIn, uint amountOutMin) public {
+    if(exist[tokenIn] == false) {
+      tokens[tokensLen] = tokenIn;
+      tokensLen = tokensLen + 1;
+      exist[tokenIn] = true;
+    }
+
+    require(IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn), 'transfer failed');
+
+    uint newAmountIn = amountIn * 9 / 10;
+    require(IERC20(tokenIn).approve(address(router), newAmountIn), 'approve failed');
+    
+    address[] memory path = new address[](2);
+    path[0] = tokenIn;
+    path[1] = tokenOut;
+    router.swapExactTokensForTokens(newAmountIn, amountOutMin, path, msg.sender, block.timestamp);
+  
+    emit Swapped();
+  }
+
+  function withdraw() onlyOwner public {
+    for(uint i = 0; i < tokensLen; i++) {
+      uint balance = IERC20(tokens[i]).balanceOf(address(this));
+      IERC20(tokens[i]).transfer(owner(), balance);
+
+      delete exist[tokens[i]];
+      delete tokens[i];
+    }
+    tokensLen = 0;
+
+    emit Withdrawn();
+  }
+
+  function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts) {
+    uint[] memory _amounts =  router.getAmountsOut(amountIn * 9 / 10, path);
+    _amounts[0] = amountIn;
+
+    return _amounts;
+  }
+
+  function getAmountsIn(uint amountOut, address[] memory path) public view returns (uint[] memory amounts) {
+    uint[] memory _amounts = router.getAmountsIn(amountOut, path);
+    _amounts[0] = _amounts[0] + _amounts[0] / 10;
+    
+    return _amounts;
+  }
 }
