7721c44a370f35323fdcb6fc0a91288694129c8c
Filip Manole <filip.manole@gmail.com>
2021-10-20 00:45:40 +0300

    Using WTH9 and UniswapV2Router02 in the tests to deploy the Swapper contract; approve swapper to use the tokens before calling addLiquidity and swap; added expand function to calculate erc20 token amounts with decimals;

--- test/Swapper.ts
+++ test/Swapper.ts
@@ -2,11 +2,13 @@ import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { Swapper, Swapper__factory, UniswapV2Factory, UniswapV2Factory__factory } from '../typechain';
+import { Swapper, Swapper__factory, UniswapV2Factory, UniswapV2Factory__factory, UniswapV2Router02, UniswapV2Router02__factory } from '../typechain';
 import { BigNumber } from '@ethersproject/bignumber';
 import { ERC20PresetMinterPauser } from '../typechain/ERC20PresetMinterPauser';
 import { ERC20PresetMinterPauser__factory } from '../typechain/factories/ERC20PresetMinterPauser__factory';
 import { ContractTransaction } from '@ethersproject/contracts';
+import { WETH9__factory } from '../typechain/factories/WETH9__factory';
+import { WETH9 } from '../typechain/WETH9';
 
 
 describe("Swapper Contract", () => {
@@ -19,11 +21,16 @@ describe("Swapper Contract", () => {
   /* Dummy tokens */
   let DT0: ERC20PresetMinterPauser;
   let DT1: ERC20PresetMinterPauser;
+
+  /* WETH token */
+  let WETH: WETH9;
   
   /* Other variables */
   let uniswapV2Factory: UniswapV2Factory;
   let swapper: Swapper;
   let tx: ContractTransaction;
+
+  const expand = (num: number): BigNumber => BigNumber.from(num).mul(BigNumber.from(10).pow(18));
 
   before(async () => {
     /* Get testing actors */
@@ -34,40 +41,48 @@ describe("Swapper Contract", () => {
     DT0 = await erc20TokenFactory.deploy("DummyToken0", "DT0");
     DT1 = await erc20TokenFactory.deploy("DummyToken1", "DT1");
 
+    /* Deploy Wrapped Ethereum */
+    const WETH9Factory = new WETH9__factory(deployer);
+    WETH = await WETH9Factory.deploy();
+
     /* Deploy UniswapV2Factory Contract */
     const uniswapV2Factory__factory = new UniswapV2Factory__factory(deployer);
     uniswapV2Factory = await uniswapV2Factory__factory.deploy(deployer.address);
 
+    /* Deploy UniswapV2Router02 Contract */
+    const uniswapV2RouterFactory = new UniswapV2Router02__factory(deployer);
+    const uniswapV2Router = await uniswapV2RouterFactory.deploy(uniswapV2Factory.address, WETH.address);
+
     /* Deploy the Swapper Contract */
     const SwapperContractFactory = new Swapper__factory(owner);
-    swapper = await SwapperContractFactory.deploy(uniswapV2Factory.address);
+    swapper = await SwapperContractFactory.deploy(uniswapV2Factory.address, uniswapV2Router.address);
   });
 
   it("should be 1000 DT0 and 1000 DT1", async () => {
     /* Assign 1000 DT0 and 1000 DT1 to the liquidity provider address */
-    tx = await DT0.mint(provider.address, 1000);
+    tx = await DT0.mint(provider.address, expand(1000));
     await tx.wait();
-    tx = await DT1.mint(provider.address, 1000);
+    tx = await DT1.mint(provider.address, expand(1000));
     await tx.wait();
 
     /* Get the provider's balances */
     const providerDT0: BigNumber = await DT0.balanceOf(provider.address);
     const providerDT1: BigNumber = await DT1.balanceOf(provider.address);
     
-    expect(providerDT0.eq(1000)).to.be.true;
-    expect(providerDT1.eq(1000)).to.be.true;
+    expect(providerDT0.eq(expand(1000))).to.be.true;
+    expect(providerDT1.eq(expand(1000))).to.be.true;
   });
 
-  it("should be 100 DT0 and 0 DT1 ", async () => {
+  it("should be 200 DT0 and 0 DT1 ", async () => {
     /* Assign 200 DT0 to the trader  */
-    tx = await DT0.mint(trader.address, 200);
+    tx = await DT0.mint(trader.address, expand(200));
     await tx.wait();
 
     /* Get the trader's balances */
     const traderDT0: BigNumber = await DT0.balanceOf(trader.address);
     const traderDT1: BigNumber = await DT1.balanceOf(trader.address);
 
-    expect(traderDT0.eq(200)).to.be.true;
+    expect(traderDT0.eq(expand(200))).to.be.true;
     expect(traderDT1.eq(0)).to.be.true;
   });
 
@@ -79,10 +94,16 @@ describe("Swapper Contract", () => {
   });
 
   it("should emit PairAdded event", async ()=> {
+    /* Approve swapper contract to use the DT0 and DT1 amount of the pair */
+    tx = await DT0.connect(provider).approve(swapper.address, expand(500));
+    await tx.wait();
+    tx = await DT1.connect(provider).approve(swapper.address, expand(500));
+    await tx.wait();
+
     /* Provider should add a pair of (DT0, DT1) in the pool */
     await expect(
-      swapper.connect(provider).addLiquidity(DT0.address, 500, DT1.address, 500)
-    ).to.emit(swapper, "PairAdded").withArgs(DT0.address, DT1.address);
+      swapper.connect(provider).addLiquidity(DT0.address, expand(500), DT1.address, expand(500))
+    ).to.emit(swapper, "PairAdded");
   });
 
   it("should be one pair in the liquidity pool", async () => {
@@ -95,11 +116,14 @@ describe("Swapper Contract", () => {
   it("should exchange DT0 for DT1", async () => {
     /* Get the received amount of DT1 when swapping 50 DT0 into DT1 */
     const path: string[] = [DT0.address, DT1.address];
-    const [amountIn, amountOut] = await swapper.connect(trader).getAmountsOut(50, path);
+    const [amountIn, amountOut] = await swapper.connect(trader).getAmountsOut(expand(50), path);
     
     /* Get trader's balances */
     const traderDT0: BigNumber = await DT0.balanceOf(trader.address);
     const traderDT1: BigNumber = await DT1.balanceOf(trader.address);
+
+    tx = await DT0.connect(trader).approve(swapper.address, amountIn);
+    await tx.wait();
 
     /* Swap 50 DT0 into DT1 */
     tx = await swapper.connect(trader).swap(DT0.address, DT1.address, amountIn, amountOut);
@@ -120,11 +144,14 @@ describe("Swapper Contract", () => {
   it("should emit Swapped event", async () => {
     /* Get the received amount of DT1 when swapping 10 DT0 into DT1 */
     const path: string[] = [DT0.address, DT1.address];
-    const [amountIn, amountOut] = await swapper.connect(trader).getAmountsOut(10, path);
+    const [amountIn, amountOut] = await swapper.connect(trader).getAmountsOut(expand(10), path);
+
+    tx = await DT0.connect(trader).approve(swapper.address, amountIn);
+    await tx.wait();
 
     await expect(
       swapper.connect(trader).swap(DT0.address, DT1.address, amountIn, amountOut)
-    ).to.emit(swapper, "Swapped").withArgs(DT1.address, amountOut);
+    ).to.emit(swapper, "Swapped");
   });
 
   it("should not withdraw if not the owner", async () => {
@@ -153,7 +180,7 @@ describe("Swapper Contract", () => {
 
     /* Calculate the new expected DT0 balnce for the owner */
     const ownerDT0: BigNumber = await DT0.balanceOf(owner.address);
-    
+
     expect(swapperDT0.eq(ownerDT0)).to.be.true;
   });
 
