f514a20e171d322c4c6577d66ac8fdfe13c7a1ed
Filip Manole <filip.manole@gmail.com>
2021-10-29 01:15:06 +0300

    Added Modal to select between tokens;

--- frontend/src/components/CustomInput.tsx
+++ frontend/src/components/CustomInput.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { Box, Button } from '@mui/material';
-
-// import { InputUnstyled } from '@mui/core';
+import {
+  Box, Button, Modal,
+} from '@mui/material';
 
 import './CustomInput.css';
 
-export interface ICustomInput {}
+export interface IToken {
+  name: string,
+  symbol: string,
+}
+
+export interface ICustomInput {
+  tokens: IToken[]
+}
 
 const boxStyle = {
   display: 'flex',
@@ -16,20 +23,70 @@ const boxStyle = {
   p: 1,
 };
 
-export const CustomInput: React.FC<ICustomInput> = () => (
-  <Box
-    sx={boxStyle}
-  >
-    <Button
-      id="symbol-button"
-      variant="contained"
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  bgcolor: 'background.paper',
+  borderRadius: 4,
+  boxShadow: 24,
+  p: 1,
+};
+
+export const CustomInput: React.FC<ICustomInput> = ({ tokens }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [token, setToken] = React.useState('Select a Token');
+
+  return (
+    <Box
+      sx={boxStyle}
     >
-      DTX
-      <KeyboardArrowDownIcon />
-    </Button>
-    <input
-      id="amount-input"
-      placeholder="0.0"
-    />
-  </Box>
-);
+      <Button
+        onClick={handleOpen}
+        id="symbol-button"
+        variant="contained"
+      >
+        {token}
+        <KeyboardArrowDownIcon />
+      </Button>
+      <input
+        id="amount-input"
+        placeholder="0.0"
+      />
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          id="select-modal"
+          sx={style}
+        >
+          {tokens.map((t) => (
+            <>
+              <Button
+                fullWidth
+                variant="contained"
+                id="content-button"
+                onClick={() => {
+                  setToken(t.symbol);
+                  handleClose();
+                }}
+              >
+                {t.symbol}
+              </Button>
+              <br />
+            </>
+          ))}
+        </Box>
+      </Modal>
+    </Box>
+  );
+};
