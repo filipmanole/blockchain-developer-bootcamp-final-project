73951f155e5f1d4923ebc5a5e885fe05db5ebbcc
Filip Manole <filip.manole@gmail.com>
2021-11-08 19:02:08 +0200

    added function to convert from bignumber representation to string;

--- frontend/src/hooks/useToken.ts
+++ frontend/src/hooks/useToken.ts
@@ -7,6 +7,7 @@ import { signerAccount } from '../states';
 export interface IUseToken {
   usable: boolean,
   expand?: (num: number) => BigNumber;
+  shrink?: (num: BigNumber) => string;
   approve?: (delegate: string, amount: BigNumber) => Promise<void>;
 }
 
@@ -20,13 +21,16 @@ const useToken = (tokenAddress: string, decimals: number = 18): IUseToken => {
   const token = ERC20PresetMinterPauser__factory.connect(tokenAddress, signer);
 
   const expand = (num: number) => BigNumber.from(num).mul(BigNumber.from(10).pow(decimals));
+  const shrink = (num: BigNumber) => num.div(BigNumber.from(10).pow(decimals)).toString();
 
   const approve = async (delegate: string, amount: BigNumber) => {
     const txn = await token.approve(delegate, amount);
     await txn.wait();
   };
 
-  return { usable: true, expand, approve };
+  return {
+    usable: true, expand, shrink, approve,
+  };
 };
 
 export default useToken;
