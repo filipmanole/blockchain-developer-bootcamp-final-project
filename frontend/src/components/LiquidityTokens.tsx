521037a57eef63d2221a22f46063d509525b807f
Filip-George Manole <filip.manole@gmail.com>
2021-11-25 20:28:07 +0200

    Remove from pool (#10)
    Implementation for removing tokens from the pool.

---
+++ frontend/src/components/LiquidityTokens.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { useAtom } from 'jotai';
+import { CircularProgress } from '@mui/material';
+import { swapperContract, TTxnStatus } from '../states';
+import LPTokenInfo from './LPTokenInfo';
+
+interface ILiquidityTokens {
+  reload: boolean
+}
+
+type TAddressName = [string, string];
+
+const divStyle:React.CSSProperties = {
+  fontFamily: 'Monospace',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+const LiquidityTokens: React.FC<ILiquidityTokens> = ({ reload }) => {
+  const [swapper] = useAtom(swapperContract);
+
+  const [txStatus, setTxStatus] = React.useState<TTxnStatus>('NOT_SUBMITED');
+  const [lpTokens, setLpTokens] = React.useState<TAddressName[]>();
+
+  const setLpTokensAndNames = async () => {
+    setTxStatus('LOADING');
+
+    const lpTokenAddresses = await swapper.getLpTokens();
+
+    const lpTokensPromises = lpTokenAddresses.map(
+      async (address):Promise<TAddressName> => [
+        address,
+        await swapper.getLpTokenName(address),
+      ],
+    );
+
+    const lp = await Promise.all(lpTokensPromises);
+    setLpTokens(lp);
+
+    setTxStatus('COMPLETE');
+  };
+
+  React.useEffect(() => {
+    if (!reload) {
+      setTxStatus('NOT_SUBMITED');
+      return;
+    }
+    setLpTokensAndNames();
+  }, [reload]);
+
+  return (
+    <>
+      {txStatus === 'LOADING' && (
+      <div style={divStyle}>
+        <CircularProgress />
+      </div>
+      )}
+      {txStatus === 'COMPLETE'
+      && (
+        <>
+          {
+            lpTokens.length === 0 ? <div style={divStyle}>No liquidity provided...</div>
+              : lpTokens.map((lpToken, i) => (
+                <div
+                /* eslint-disable-next-line */
+                key={i}
+                  style={divStyle}
+                >
+                  {lpToken[1]}
+                &nbsp;&nbsp;
+                  <LPTokenInfo lpTokenAddress={lpToken[0]} />
+                </div>
+              ))
+          }
+        </>
+      )}
+    </>
+  );
+};
+
+export default LiquidityTokens;
